2adf57c319b1f0dd710059032d552d17ce902c74
unknown <bhagyashree.kurane@gmail.com>
2022-11-01 19:49:45 +0530

    Commiting all files

---
+++ Iris_API.py
@@ -0,0 +1,28 @@
+from distutils.log import debug
+from flask import Flask ,render_template,request,jsonify
+import numpy as np
+import pickle
+from Iris_Project_Data.utils import IrisSpeciesPrediction
+import config
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template("home.html")
+
+@app.route('/predict_spieces', methods = ['POST'])
+def final_predict_spieces():
+    SepalLengthCm = float(request.form["SepalLengthCm"])
+    SepalWidthCm =float(request.form["SepalWidthCm"]) 
+    PetalLengthCm = float(request.form["PetalLengthCm"])
+    PetalWidthCm = float(request.form["PetalWidthCm"])
+
+    Obj_Species = IrisSpeciesPrediction(SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm)
+    prediction = Obj_Species.get_predict_spieces()
+
+    return render_template("after.html",prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,5 @@
+import os 
+
+PORT_NUMBER = 5003
+MODEL_FILE_PATH = os.path.join("Iris_Project_Data","Linear_Model.pkl")
+JSON_FILE_PATH = os.path.join("Iris_Project_Data","Project_Data.json")
