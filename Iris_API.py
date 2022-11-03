d199706f129c14faaafebe01cefa42ca3f4959c6
unknown <bhagyashree.kurane@gmail.com>
2022-11-03 16:00:43 +0530

    API file modified

--- Iris_API.py
+++ Iris_API.py
@@ -25,4 +25,4 @@ def final_predict_spieces():
     return render_template("after.html",prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host ="0.0.0.0",port=8080,debug=True)
